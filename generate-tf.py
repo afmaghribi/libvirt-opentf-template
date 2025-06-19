aa86194feac40c85fc5be76a8b8e2f01d87cb676
afmaghribi <af.maghribi@gmail.com>
2025-06-20 03:49:56 +0700

    fix add generate cloud-init file function

--- generate-tf.py
+++ generate-tf.py
@@ -4,6 +4,24 @@ import os, yaml
 from jinja2 import Environment,FileSystemLoader
 from bcrypt import hashpw,gensalt
 
+def render_cloud_init(specs):
+    template_file = Environment(loader=FileSystemLoader("./templates")).get_template("user-data.j2")
+    
+    for vms in (specs['vm_specs']):
+        vms['hashed_password'] = hashpw(vms['password'].encode(), gensalt()).decode()
+
+        if "pub_key" not in specs.keys():
+            key_path = os.path.expanduser('~/.ssh/id_rsa.pub')
+            vms["pub_key"] = open(key_path,"r").read().strip()
+            vms["priv_key"] = key_path[:-4]
+        elif "ssh-rsa" not in vms["pub_key"]:
+            vms["pub_key"] = open(vms["pub_key"],"r").read().strip()
+            vms["priv_key"] = vms["pub_key"][:-4]
+
+        output = open("./"+vms['hostname']+".cfg","w")
+        output.write(template_file.render(vms))
+        output.close()
+
 def render_tf_file(specs):
     template_file = Environment(loader=FileSystemLoader("./templates")).get_template("main.tf.j2")
     
@@ -13,4 +31,5 @@ def render_tf_file(specs):
 
 if __name__ == "__main__": 
     config = yaml.safe_load(open("tf-specs.yaml","r"))
+    render_cloud_init(config['tf_specs'])
     render_tf_file(config['tf_specs'])
